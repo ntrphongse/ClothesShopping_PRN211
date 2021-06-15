fd4ae2968dbe661932e568793290a70256920ff8
ntrphongse <n.tranphongse@gmail.com>
2021-06-15 21:27:25 +0700

    Very First Commit
    Create ClothesShoppingLibrary and ClothesShoppingWebApp (with ASP.NET Core Wep App (MVC))

---
+++ ClothesShoppingWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ClothesShoppingWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
