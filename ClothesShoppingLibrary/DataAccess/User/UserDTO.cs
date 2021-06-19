7c2b4af0b37b09da0bf016718c6cd36963ed70a7
ntrphongse <n.tranphongse@gmail.com>
2021-06-19 10:05:54 +0700

    Update DataObject files to DTO
    Packagize classes and change name to DTO format

---
+++ ClothesShoppingLibrary/DataAccess/User/UserDTO.cs
@@ -0,0 +1,31 @@
+﻿using ClothesShoppingLibrary.DataAccess.Order;
+using ClothesShoppingLibrary.DataAccess.Role;
+using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace ClothesShoppingLibrary.DataAccess.User
+{
+    public partial class UserDTO
+    {
+        public UserDTO()
+        {
+            Orders = new HashSet<OrderDTO>();
+        }
+
+        public int UserId { get; set; }
+        public string FullName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string Password { get; set; }
+        public bool? Gender { get; set; }
+        public DateTime? Birthday { get; set; }
+        public int Role { get; set; }
+        public bool Status { get; set; }
+
+        public virtual RoleDTO RoleNavigation { get; set; }
+        public virtual ICollection<OrderDTO> Orders { get; set; }
+    }
+}
