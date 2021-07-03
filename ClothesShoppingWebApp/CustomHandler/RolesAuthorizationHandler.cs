a3b0492506ecee757bf0c6088df14ef9b2ba2450
ntrphongse <n.tranphongse@gmail.com>
2021-07-03 11:37:51 +0700

    Authorization using Policy and Role-based

---
+++ ClothesShoppingWebApp/CustomHandler/RolesAuthorizationHandler.cs
@@ -0,0 +1,44 @@
+﻿using DTOLibrary;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authorization.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClothesShoppingWebApp.CustomHandler
+{
+    public class RolesAuthorizationHandler : AuthorizationHandler<RolesAuthorizationRequirement>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, RolesAuthorizationRequirement requirement)
+        {
+            if (context.User == null || !context.User.Identity.IsAuthenticated)
+            {
+                context.Fail();
+                return Task.CompletedTask;
+            }
+
+            var validRole = false;
+            if (requirement.AllowedRoles == null || requirement.AllowedRoles.Any() == false)
+            {
+                validRole = true;
+            }
+            else
+            {
+                var claims = context.User.Claims;
+                var userId = claims.FirstOrDefault(c => c.Type == "UserId").Value;
+                var roles = requirement.AllowedRoles;
+
+                validRole = new lPVNgP26wKContext().Users.Where(us => roles.Contains(us.RoleNavigation.RoleName) && us.UserId == int.Parse(userId)).Any();
+            }
+            if (validRole)
+            {
+                context.Succeed(requirement);
+            } else
+            {
+                context.Fail();
+            }
+            return Task.CompletedTask;
+        }
+    }
+}
--- ClothesShoppingWebApp/Startup.cs
+++ ClothesShoppingWebApp/Startup.cs
@@ -1,4 +1,6 @@
+using ClothesShoppingWebApp.CustomHandler;
 using DTOLibrary;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
@@ -8,6 +10,7 @@ using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace ClothesShoppingWebApp
@@ -24,8 +27,33 @@ namespace ClothesShoppingWebApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Authorization
+            services.AddAuthentication("CookieAuthentication")
+                .AddCookie("CookieAuthentication", config =>
+                {
+                    config.Cookie.Name = "UserLoginCookie";
+                    config.LoginPath = "/Login";
+                    config.AccessDeniedPath = "/Login/UserAccessDenied";
+                });
+            services.AddAuthorization(config =>
+            {
+                config.AddPolicy("UserPolicy", policyBuilder =>
+                {
+                    policyBuilder.UserRequireCustomClaim(ClaimTypes.Email);
+
+                });
+            });
+            services.AddScoped<IAuthorizationHandler, PoliciesAuthorizationHandler>();
+            services.AddScoped<IAuthorizationHandler, RolesAuthorizationHandler>();
             
             services.AddControllersWithViews();
+
+            // Using Session
+            services.AddDistributedMemoryCache(); // Store Cache in Memory (Session uses this)
+            services.AddSession();
+
+            // Session for other
+            services.AddHttpContextAccessor();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,9 +67,14 @@ namespace ClothesShoppingWebApp
             {
                 app.UseExceptionHandler("/Home/Error");
             }
+            app.UseHttpsRedirection();
             app.UseStaticFiles();
 
+            app.UseSession(); // Register to use Session
+
             app.UseRouting();
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
